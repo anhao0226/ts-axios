1f8b05be3ab475ad019d3cc583c86bf8d6ba5802
hangiangai <2919099739@qq.com>
2020-09-17 17:09:20 +0800

    [build] 1.0.0

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -3,65 +3,65 @@ import { parseHeaders } from '../helpers/headers'
 import { createError } from '../helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise((resolve: any, reject: any) => {
-    const { data = null, url, method = 'get', headers, responseType, timeout } = config
-    const request = new XMLHttpRequest()
-    // 响应值类型
-    if (responseType) {
-      request.responseType = responseType
-    }
-    if (timeout) {
-      request.timeout = timeout
-    }
-    request.open(method.toLocaleUpperCase(), url!, true)
-    // 监听状态变化
-    request.onreadystatechange = function handleLoad() {
-      if (request.readyState != 4) return
-      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
-      const responseData = responseType !== 'text' ? request.response : request.responseText
-      const response: AxiosResponse = {
-        data: responseData,
-        status: request.status,
-        statusText: request.statusText,
-        headers: responseHeaders,
-        config,
-        request
-      }
-      if (request.status === 0) return
-      handleResponse(response)
-    }
-    // 错误处理
-    request.onerror = function handleError() {
-      reject(createError('Network Error', config, null, request))
-    }
-    // 超时处理
-    request.ontimeout = function handleTimeout() {
-      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
-    }
+    return new Promise((resolve: any, reject: any) => {
+        const { data = null, url, method = 'get', headers, responseType, timeout } = config
+        const request = new XMLHttpRequest()
+        // 响应值类型
+        if (responseType) {
+            request.responseType = responseType
+        }
+        if (timeout) {
+            request.timeout = timeout
+        }
+        request.open(method.toLocaleUpperCase(), url!, true)
+        // 监听状态变化
+        request.onreadystatechange = function handleLoad() {
+            if (request.readyState !== 4) return
+            const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+            const responseData = responseType !== 'text' ? request.response : request.responseText
+            const response: AxiosResponse = {
+                data: responseData,
+                status: request.status,
+                statusText: request.statusText,
+                headers: responseHeaders,
+                config,
+                request
+            }
+            if (request.status === 0) return
+            handleResponse(response)
+        }
+        // 错误处理
+        request.onerror = function handleError() {
+            reject(createError('Network Error', config, null, request))
+        }
+        // 超时处理
+        request.ontimeout = function handleTimeout() {
+            reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
+        }
 
-    Object.keys(headers).forEach(name => {
-      if (data === null && name.toLowerCase() == 'content-type') {
-        delete headers[name]
-      } else {
-        request.setRequestHeader(name, headers[name])
-      }
+        Object.keys(headers).forEach(name => {
+            if (data === null && name.toLowerCase() === 'content-type') {
+                delete headers[name]
+            } else {
+                request.setRequestHeader(name, headers[name])
+            }
+        })
+        request.send(data)
+
+        function handleResponse(response: AxiosResponse): void {
+            if (response.status >= 200 && response.status < 300) {
+                resolve(response)
+            } else {
+                reject(
+                    createError(
+                        `Request failed with status code ${response.status}`,
+                        config,
+                        null,
+                        request,
+                        response
+                    )
+                )
+            }
+        }
     })
-    request.send(data)
-
-    function handleResponse(response: AxiosResponse): void {
-      if (response.status >= 200 && response.status < 300) {
-        resolve(response)
-      } else {
-        reject(
-          createError(
-            `Request failed with status code ${response.status}`,
-            config,
-            null,
-            request,
-            response
-          )
-        )
-      }
-    }
-  })
 }
